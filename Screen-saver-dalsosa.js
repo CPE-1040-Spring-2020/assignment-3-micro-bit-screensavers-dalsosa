3577e924072dad479c9c21b43f3a3e76b474818f
dalsosa <60205412+dalsosa@users.noreply.github.com>
2020-02-17 17:55:01 -0700

    Update Screen-saver-dalsosa.js

--- Screen-saver-dalsosa.js
+++ Screen-saver-dalsosa.js
@@ -15,22 +15,122 @@ basic.forever(function () {
             led.enable(true)
         })
     }
+    function dots() {
+        dot1.move(1);
+        basic.pause(100)
+        dot2.move(-1);
+        dot2.turn(Direction.Right, 90)
+        basic.pause(500)
+        dot3.turn(Direction.Right, -45)
+        dot3.move(1);
+        basic.pause(2000)
+        dot1.ifOnEdgeBounce();
+        dot1.turn(Direction.Right, 45)
+        dot2.ifOnEdgeBounce();
+        dot2.turn(Direction.Right, 45)
+        dot3.ifOnEdgeBounce();
+        dot3.turn(Direction.Right, 45)
+    }
+    function jump() {
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            # . # . #
+            . . # . .
+            . # . # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            . . # . .
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(300)
+
+    }
+    function wave() {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(300)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            . # . . .
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            # # # . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . . . # .
+            . . . . #
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(200)
+    }
+    function wink() {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(300)
+        basic.showLeds(`
+            . . . # .
+            # # . # .
+            . . . . .
+            # . . . .
+            . # # # .
+            `)
+        basic.pause(300)
+    }
 
 
-})
-basic.forever(function () {
-    dot1.move(1);
-    basic.pause(100)
-    dot2.move(-1);
-    dot2.turn(Direction.Right, 60)
-    basic.pause(500)
-    dot3.turn(Direction.Right, -45)
-    dot3.move(1);
-    basic.pause(2000)
-    dot1.ifOnEdgeBounce();
-    dot1.turn(Direction.Right, 45)
-    dot2.ifOnEdgeBounce();
-    dot2.turn(Direction.Right, 45)
-    dot3.ifOnEdgeBounce();
-    dot3.turn(Direction.Right, 45)
+    input.onGesture(Gesture.Shake, function () {
+        while (asleep == false)
+            dots()
+
+    })
+    input.onLogoDown(function () {
+        while (asleep == false)
+            jump()
+
+    })
+    input.onLogoUp(function () {
+        while (asleep == false)
+            wave()
+    })
+    input.onButtonPressed(Button.AB, function () {
+        while (asleep == false)
+            wink()
+    })
+
+
+
 })

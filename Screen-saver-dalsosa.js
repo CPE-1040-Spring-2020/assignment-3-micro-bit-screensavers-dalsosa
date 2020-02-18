ee206b437db5afc94a8a2d00379a9daa6061dcb9
dalsosa <60205412+dalsosa@users.noreply.github.com>
2020-02-17 19:29:10 -0700

    Update Screen-saver-dalsosa.js

--- Screen-saver-dalsosa.js
+++ Screen-saver-dalsosa.js
@@ -1,162 +1,171 @@
 let dot1 = game.createSprite(2, 2)
-let dot2 = game.createSprite(1, 2)
+let dot2 = game.createSprite(2, 2)
 let dot3 = game.createSprite(2, 2)
 let asleep: boolean = false
+let g: Gesture
+
 input.onButtonPressed(Button.A, function () {
     asleep = true
 })
-
-basic.forever(function () {
-    if (asleep) {
-        led.enable(false)
-        asleep = false
-    } else {
-        input.onButtonPressed(Button.B, function () {
-            led.enable(true)
-        })
-    }
-    function dots() {
-        dot1.move(1);
-        basic.pause(100)
-        dot2.move(-1);
-        dot2.turn(Direction.Right, 90)
-        basic.pause(500)
-        dot3.turn(Direction.Right, -45)
-        dot3.move(1);
-        basic.pause(2000)
-        dot1.ifOnEdgeBounce();
-        dot1.turn(Direction.Right, 45)
-        dot2.ifOnEdgeBounce();
-        dot2.turn(Direction.Right, 45)
-        dot3.ifOnEdgeBounce();
-        dot3.turn(Direction.Right, 45)
-    }
-    function jump() {
-        basic.showLeds(`
+input.onButtonPressed(Button.B, function () {
+    asleep = false
+})
+input.onGesture(Gesture.Shake, function () {
+    g = Gesture.Shake
+})
+input.onLogoDown(function () {
+    g = Gesture.LogoDown
+})
+input.onLogoUp(function () {
+    g = Gesture.LogoUp
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    g = Gesture.TiltLeft
+})
+input.onGesture(Gesture.TiltRight, function () {
+    g = Gesture.TiltRight
+})
+function dots() {
+    dot1.move(1);
+    basic.pause(100)
+    dot2.move(-1);
+    dot2.turn(Direction.Right, 90)
+    basic.pause(500)
+    dot3.turn(Direction.Right, -45)
+    dot3.move(1);
+    basic.pause(2000)
+    dot1.ifOnEdgeBounce();
+    dot1.turn(Direction.Right, 45)
+    dot2.ifOnEdgeBounce();
+    dot2.turn(Direction.Right, 45)
+    dot3.ifOnEdgeBounce();
+    dot3.turn(Direction.Right, 45)
+}
+function jump() {
+    basic.showLeds(`
             . # # # .
             . # # # .
             # . # . #
             . . # . .
             . # . # .
             `)
-        basic.pause(100)
-        basic.showLeds(`
+    basic.pause(100)
+    basic.showLeds(`
             . # # # .
             # # # # #
             . . # . .
             # # # # #
             . . . . .
             `)
-        basic.pause(300)
-    }
-    function wave() {
-        basic.showLeds(`
+    basic.pause(300)
+}
+function wave() {
+    basic.showLeds(`
             . . . . .
             . . . . .
             . . . . .
             . . . . .
             . . . . .
             `)
-        basic.pause(300)
-        basic.showLeds(`
+    basic.pause(300)
+    basic.showLeds(`
             . . . . .
             . . . . .
             . . . . .
             # . . . .
             . # . . .
             `)
-        basic.pause(200)
-        basic.showLeds(`
+    basic.pause(200)
+    basic.showLeds(`
             # # # . .
             . . . # .
             . . # . .
             . # . . .
             . . # . .
             `)
-        basic.pause(200)
-        basic.showLeds(`
+    basic.pause(200)
+    basic.showLeds(`
             . . . . #
             . . . # .
             . . # . .
             . . . # .
             . . . . #
             `)
-        basic.pause(200)
-        basic.showLeds(`
+    basic.pause(200)
+    basic.showLeds(`
             . . . . .
             . . . . .
             . . . . #
             . . . . .
             . . . . .
             `)
-        basic.pause(200)
-    }
-    function wink() {
-        basic.showLeds(`
+    basic.pause(200)
+}
+function wink() {
+    basic.showLeds(`
             . # . # .
             . # . # .
             . . . . .
             # . . . #
             . # # # .
             `)
-        basic.pause(300)
-        basic.showLeds(`
+    basic.pause(300)
+    basic.showLeds(`
             . . . # .
             # # . # .
             . . . . .
             # . . . .
             . # # # .
             `)
-        basic.pause(300)
-    }
-    function FerrisWheel(){
-        basic.showLeds(`
+    basic.pause(300)
+}
+function FerrisWheel() {
+    basic.showLeds(`
             . # # # .
             # . # . #
             # # # # #
             # . # . #
             . # # # .
             `)
-        basic.pause(500)
-        basic.showLeds(`
+    basic.pause(500)
+    basic.showLeds(`
             . # . # .
             # . # . #
             . # . # .
             # . # . #
             . # . # .
             `)
-        basic.pause(500)
-        basic.showLeds(`
+    basic.pause(500)
+    basic.showLeds(`
             . . # . .
             . . . . .
             # . # . #
             . . . . .
             . . # . .
             `)
-        basic.pause(500)
+    basic.pause(500)
 
+}
+basic.forever(function () {
+    if (asleep) {
+       if (g==Gesture.Shake) {
+           dots()
+       }
+       else if (g==Gesture.LogoDown) {
+           FerrisWheel()
+       }
+       else if (g==Gesture.LogoUp){
+           wave()
+       }
+       else if (g==Gesture.TiltLeft){
+           jump()
+       }
+       else if (g==Gesture.TiltRight){
+           wink()
+       }
+    } else {
+        basic.showString("Awake")
     }
-
-    input.onGesture(Gesture.Shake, function () {
-        while (asleep == false)
-            dots()
-    })
-    input.onLogoDown(function () {
-        while (asleep == false)
-            jump()
-    })
-    input.onLogoUp(function () {
-        while (asleep == false)
-            wave()
-    })
-    input.onButtonPressed(Button.AB, function () {
-        while (asleep == false)
-            wink()
-    })
-    input.onGesture(Gesture.TiltRight, function () {
-        while (asleep == false)
-            FerrisWheel()
-    })
 })
 
 
